1d79bc2eb36d4f8c7855570cf3940c16bae69945
Jucélio silva santos <89044629+JucelioSilvaDigital@users.noreply.github.com>
2021-08-26 15:59:50 -0400

    Add files via upload

---
+++ dados/produtos.php
@@ -0,0 +1,22 @@
+<?php
+$produtos = array(
+    "1" => array("Bolo Caseiro Tigreza", "20.00", "BOLO-TIGREZA.jpg", "400g de um Delicioso bolo caseiro"),
+    "2" => array("Bolo Caseiro 3 Leites", "25.00", "BOLO-3-LEITES.jpg", "800g de um Delicioso bolo caseiro a base de 3 leites"),
+    "3" => array("BOLO CASEIRO TOALHA FELPUDA", "35.00", "BOLO-TOALHA-FELPUDA.jpg", "300g de um Delicioso bolo caseiro que é a sensação do memento"),
+    "4" => array("BRIGADEIRO NINHO COM NUTELLA", "1.40", "Ninho-com-Nutella-1.jpg", "Brigadeiro caseiro com a combinação perfeita"),
+    "5" => array("BRIGADEIRO MISTO", "1.05", "brigadeiro-misto2.jpg", "Que tal misturar os sabores?"),
+    "6" => array("BRIGADEIRO DE LIMÃO", "1.27", "Limao-2.jpg", "Um azedinho na quantidade certa é a chave..."),
+    "7" => array("BRIGADEIRO DE CHURROS ENC.", "1.17", "Churro-2.jpg", "Este é diferente... brigadeiro com sabor de churros"),
+    "8" => array("CARAMELADO UVA PASSAS", "1.50", "caramelado-uva-passas2.jpg", "Que tal uma cobertura com uva passas e delicioso caramelo?"),
+    "9" => array("CARAMELADO UVA", "1.63", "caramelado-uva.jpg", "Com o sabor da fruta... hummmm!"),
+    "10" => array("CARAMELADO TÂMARA", "2.00", "caramelado-tamara.jpg", "O mais delicioso de todos"),
+    "11" => array("Festive Pocket Premium", "75.00", "BAnner-produto-festive-pocket-premium.jpg", "Pacote para festa com nosso basico e minimo de 20 unidade para escolher"),
+    "12" => array("Festive Premium", "110.00", "BAnner-produto-festive-premium.jpg", "Pacote intermediario 1 para festa"),
+    "13" => array("Festive Pocket", "147.50", "BAnner-produto-festive-pocket.jpg", "Pacote tope para festas"),
+    "14" => array("Festive Mini", "98.00", "BAnner-produto-festive-mini.jpg", "Pacote intermediario 2 para festas"),
+    "15" => array("Caixa pra Ele", "150.00", "Para-eles.jpg", "Este presente é especial para ele..."),
+    "16" => array("BRIGADEIRO DE PAÇOCA", "1.35", "IMG-20200618-WA0042.jpg", "Já pensou em misturar?")
+);
+
+
+$qtd = count($produtos);
