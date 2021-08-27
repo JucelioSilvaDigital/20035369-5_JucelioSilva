1ab470951ae0f72039712f157e2df7a217a5925f
Jucélio silva santos <89044629+JucelioSilvaDigital@users.noreply.github.com>
2021-08-26 21:38:43 -0400

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,48 @@
+<!-- Footer -->
+<footer class="page-footer font-small btn-unique fixed-bottom">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Grid row-->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-12 py-5">
+        <div class="mb-5 flex-center">
+
+          <!-- Facebook -->
+          <a href="https://www.facebook.com/JucelioSilvaDigital" class="fb-ic">
+            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          
+          <!-- Google +-->
+          <a href="https://www.youtube.com/channel/UC6tc3wVSVy3EiZYIyXR5BEA" class="gplus-ic">
+            <i class="fab fa-youtube-square fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          
+          <!--Instagram-->
+          <a href="https://www.instagram.com/juceliosilvadigital/" class="ins-ic">
+            <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          
+        </div>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">
+      © <?= date('Y'); ?> Copyright: Jucélio Silva Santos
+      <br>
+      Aluno Unicesumar RA: 20035369-5
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
---
+++ includes/menu.php
@@ -0,0 +1,15 @@
+<ul class="nav md-tabs btn-unique mx-0 mb-0 mt-1">
+    <li class="nav-item">
+        <a class="nav-link" href="/">Inicio</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/quem-somos">Quem Somos?</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/contato">Contato</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/local">Localização</a>
+    </li>
+</ul>
+<br>    
