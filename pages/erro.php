5ed3572fd282d11d6e444e3103a8552d2aba8bf3
Jucélio silva santos <89044629+JucelioSilvaDigital@users.noreply.github.com>
2021-08-26 21:39:33 -0400

    Add files via upload

---
+++ pages/erro.php
@@ -0,0 +1,8 @@
+<H1 class="textcenter">Erro...</H1>
+<article class="article center">
+
+  <h2 class="article__title">Página não existe ou foi removida</h2>
+  <div class="article__date">Data: <?= date("d/m/Y - H:i:s"); ?></div>
+  <div class="article__content">Verifique a URL digitada...
+  </div>
+</article>
